88b51c1ba3fb1b3190f6259a3407a61612e8e836
Vít Ježek <jezek.vit99@gmail.com>
2021-11-06 22:33:52 +0100

    oprava_1

--- piskvorky.py
+++ piskvorky.py
@@ -1,96 +1,123 @@
-from turtle import circle, forward, left, right, exitonclick, speed, penup, pendown, goto, color, pensize  # import potřebných příkazů
+from turtle import circle, forward, left, right, exitonclick, speed, penup, pendown, goto, color, pensize 
 
-ctverec = 50                                                # konstanta délky strany čtverce
-while(True):                                                # pracujeme s True kvůli isnumeric, 
-    radek = (input('Zadej počet řádků:'))                   # nejdříve zadáváme počet řádků
-    if not radek.isnumeric():                               # ošetření proti zadání písmena je vytvořeno pomocí isnumeric, který vrací True, pokud jsou zadané číselné znaky.  
+strana_ctverce = 50                                       
+while(True):                                                
+    radek = (input('Zadej počet řádků:'))
+    
+    # ošetření proti zadání písmena                
+    if not radek.isnumeric():                               
         print("Zkus to znovu a zadej celé kladné číslo")    
-        continue                                            # cyklus se opakuje dokud uživatel nezadá správnou hodnotu
+        continue                                          
 
-    radek = int(radek)                                      # přepis proměnné na intiger (do isnumeric můžeme totiž zapsat i jiné hodnoty než celá čísla a to my nechceme)
-    if(radek == 0):                                         # ošetření proti zadání nuly
-        print("Zkus to znovu a zadej célé kladné číslo")  
+    radek = int(radek)                                      
+    
+    # ošetření proti zadání nuly
+    if(radek == 0):                                         
+        print("Zkus to znovu a zadej celé kladné číslo")  
     else:
-        break                                               # ukončení cyklu
+        break                                            
 
-while(True):                                                # stejný postup je použit i u sloupců
+while(True):                                                
     sloupec = (input('Zadej počet sloupců:'))
+    
+    # ošetření proti zadání písmena 
     if not sloupec.isnumeric():
         print("Zkus to znovu a zadej celé kladné číslo")
         continue
 
     sloupec = int(sloupec)
+    
+    # ošetření proti zadání nuly
     if(sloupec == 0):
-        print("Zkus to znovu a zadej célé kladné číslo")
+        print("Zkus to znovu a zadej celé kladné číslo")
     else:
         break
 
+# vykreslení hracího pole
 speed(0)
 pensize (3)
-for _ in range(radek):                                     # na následujících řádcích je zapsán princip vykreslení hracího pole
-    for _ in range(sloupec):                               # pracujeme s konstantami radek a sloupec, kvůli různým velikostem pole podle zadání uživatele
+for _ in range(radek):                                     
+    for _ in range(sloupec):                            
         for _ in range(4):
-            forward(ctverec)
+            forward(strana_ctverce)
             left(90)
-        forward(ctverec)
+        forward(strana_ctverce)
     left(180)
-    forward(sloupec*ctverec)
+    forward(sloupec*strana_ctverce)
     right(90)
-    forward(ctverec)
+    forward(strana_ctverce)
     right(90)
 
+print("Souřadnice jsou číselné pro osu x a pro osu y zvlášt, levé horní pole je označeno jako 1X 1Y, pravé dolní pole jako nX a nY")
 
-hrac = True                                                # zadání proměnné hráč (střídání hráčů funguje na principů střídání hodnot True a False - zasadní je pro to řádek 93)                                            
+hrac = True                                                                                           
 
-for _ in range(sloupec*radek):                             # zde je zapsán počet tahů ve for cyklu, tedy celkový počet polí
-    if(hrac):                                              # vypisujeme který hráč je na tahu (následuje výběř souřadnic)
+# hlavní herní smyčka
+for _ in range(sloupec*radek):                             
+    
+    # vypisujeme který hráč je na tahu
+    if(hrac):                                              
         print("Hraje hráč X")
     else:
         print("Hraje hráč O")
-    while(True):                                           # princip zápisu souřadnic je stejný jako u hracího pole (první hraje hráč X, protože je hodnocen jako True)                                 
-        x = input("Zadej souřadnici x: ")                  # souřadnice jsou číselné pro osu x a pro osu y zvlášt, levé horní pole je označeno jako 1X 1Y, pravé dolní pole jako nX a nY
-        if not x.isnumeric():                              # opět pracujeme s isnumeric
+    
+    # výběr souřadnice x
+    while(True):                                                                           
+        x = input("Zadej souřadnici x: ")                  
+        if not x.isnumeric():                              
             print("Zkus to znovu a zadej správnou souřadnici")
             continue
 
         x = int(x)
-        if(x <= 0 or x > sloupec):                         # ošetření proti zadání čísel, které nejsou souřadnicemi. Tedy menší a stejné 0 a větší než počet sloupců
+
+        # ošetření proti zadání čísel, které nejsou souřadnicemi
+        if(x <= 0 or x > sloupec):                         
             print("Zkus to znovu a zadej správnou souřadnici")
         else:
             break
-
-    while(True):                                           # stejný postup pro y
+    
+    # výběr souřadnice y
+    while(True):                                           
         y = input("Zadej souřadnici y: ")
         if not y.isnumeric():
             print("Zkus to znovu a zadej správnou souřadnici")
             continue
 
         y = int(y)
+
+        # ošetření proti zadání čísel, které nejsou souřadnicemi
         if(y <= 0 or y > radek):
             print("Zkus to znovu a zadej správnou souřadnici")
         else:
             break
     
-    poziceX = (x-1)*ctverec                                # zapsány jsou také dvě proměnné pro posun pera z nulových zouřadnic na souřadnice vybraného pole
-    poziceY = (radek+1-y)*ctverec                
+
+    # zapsány jsou také dvě proměnné pro posun pera z nulových souřadnic na souřadnice vybraného pole
+    poziceX = (x-1)*strana_ctverce                       
+    poziceY = (radek+1-y)*strana_ctverce                
     
-    if hrac:                                               # vykreslujeme pole hráče x (stále jsme pod prvním for cyklem)
-        color('blue')                                      # v tomto případě zakreslujeme křížek
+    # vykreslení pole hráče X
+    if hrac:                                              
+        color('blue')                                    
         penup()
         goto(poziceX, poziceY)                         
         pendown()
-        goto(poziceX+ctverec, poziceY-ctverec)            
+        goto(poziceX+strana_ctverce, poziceY-strana_ctverce)            
         penup()
-        goto(poziceX+ctverec, poziceY)
+        goto(poziceX+strana_ctverce, poziceY)
         pendown()
-        goto(poziceX, poziceY-ctverec)
-    else:                                                 # přes else se dostaneme i k druhému hráči, který zakresluje kruh
+        goto(poziceX, poziceY-strana_ctverce)
+    
+    # vykreslení pole hráče O
+    else:                                             
         color('red')
         penup()
-        goto(poziceX+(ctverec/2), poziceY-ctverec+4)
+        goto(poziceX+(strana_ctverce/2), poziceY-strana_ctverce+4)
         pendown()
-        circle((ctverec/2)-4)
-    hrac = not hrac                                       # tento řádek je zásadní pro výměnu hráčů (otočíme totiž hodnoty true a false pro proměnnou hrac)                                 
+        circle((strana_ctverce/2)-4)
+    
+    # změna hráče
+    hrac = not hrac                                       
 
-print("Konec hry")                                        # po ukončení cyklu (počet tahů podle počtu polí vypíšeme konec hry)
+print("Konec hry")                                    
 exitonclick()
