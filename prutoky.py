55345a86af5bf74f92485bc7f871b785f4544310
Vít Ježek <jezek.vit99@gmail.com>
2021-11-25 00:45:45 +0100

    7 denni prutoky

--- prutoky.py
+++ prutoky.py
@@ -4,7 +4,10 @@ from os import read
 def get_rows(ctecka):
     bag = []
     for x in range(7):
-        row = next(ctecka) 
+        try:
+            row = next(ctecka)
+        except StopIteration:
+            break 
         bag.append (row)
     return bag
 
@@ -13,16 +16,19 @@ def average(seznam):
     for x in seznam:
         sum = sum + float(x[-1])
     return sum/len(seznam)
-    
-    
-    
-    
+
+
    
-with open('vstup.csv', 'r')  as inputfile:
+with open('vstup.csv', 'r')  as inputfile, open('vystup_7dni.csv', 'w', newline ='')  as outputfile:
    reader = csv.reader(inputfile)
-   rows = get_rows(reader)
-   average(rows)
-
-
-
+   writer = csv.writer(outputfile)
+   while True:
+        rows = get_rows(reader)
+        if len(rows) == 0:
+            break
+        result = average(rows)
+        b = rows[0][0:5] 
+        b.append(round(result,4))
+    
+        writer.writerow(b)
 
