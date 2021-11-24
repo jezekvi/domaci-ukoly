8a1aabcd78d9932d81a85157f2bcfaa85b706646
Vít Ježek <jezek.vit99@gmail.com>
2021-11-24 23:19:51 +0100

    jedu funkce

--- prutoky.py
+++ prutoky.py
@@ -1,4 +1,28 @@
 import csv
+from os import read
 
+def get_rows(ctecka):
+    bag = []
+    for x in range(7):
+        row = next(ctecka) 
+        bag.append (row)
+    return bag
+
+def average(seznam):
+    sum = 0
+    for x in seznam:
+        sum = sum + float(x[-1])
+    return sum/len(seznam)
+    
+    
+    
+    
+   
 with open('vstup.csv', 'r')  as inputfile:
    reader = csv.reader(inputfile)
+   rows = get_rows(reader)
+   average(rows)
+
+
+
+
