e7fd394a9d3021e28636221df4a16e4c1b618504
Vít Ježek <jezek.vit99@gmail.com>
2021-12-15 00:38:05 +0100

    delete ignored

--- bin_search.py
+++ bin_search.py
@@ -1,5 +1,16 @@
-bin_search.py
-cv_9.py
-cv_10.py
-džejson.py
-rekurze.py
+alist = ("a", "c", "d", "f", "f", "h", "t")
+
+
+def find(alist, value, left, right):
+    while(left <= right):
+        mid = (right//2)
+        if alist[mid] == value:
+            return True
+        elif value < alist[mid]:
+            right = mid - 1
+        else:
+            left = alist[mid] + 1
+        return False
+
+a = find(alist, 'h', 0, len(alist))
+print(a)
